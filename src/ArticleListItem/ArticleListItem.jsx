19f64eb260fc141daeebda27c85ffbb122ac0065
Gavin Chen <gavin.yiwei@gmail.com>
2020-10-25 18:02:23 -0700

    css-assignment

--- src/ArticleListItem/ArticleListItem.jsx
+++ src/ArticleListItem/ArticleListItem.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
+import ArticleTextToggleButton from "../ArticleTextToggleButton/ArticleTextToggleButton";
+import styles from "./ArticleListItem.module.css"
 
 const ArticleListItem = (props) => {
+  const[btnText, setBtnText] = useState("Show more");
+  function onClick() {
+    if(btnText === "Show more") {
+      setBtnText("Show less")
+    } else {
+      setBtnText("Show more")
+    }
+  }
   return (
-    <li>
-      <article>
-        <h2>{props.article.title}</h2>
-        <time dateTime={props.article.timeStamp}>
-          {props.article.displayDate}
-        </time>
-        <p>{props.article.shortText}</p>
-      </article>
-    </li>
+    <div className={styles.container}>
+      <h1 className={styles.title}>{props.article.title}</h1>
+        {
+          btnText === "Show less" ?
+            <div>
+                <p className={styles.shortText}>{props.article.shortText}</p>
+                <time className={styles.time} dateTime={props.article.timeStamp}>{props.article.displayDate}</time>
+            </div>  : null
+        }
+        <ArticleTextToggleButton onClick = {onClick} buttonText = {btnText}/>
+    </div>
   );
 };
 
